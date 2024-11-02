9d5cf8d3c012163b7d8916bc820c4326d77d3f1b
jcardus <joaquim_cardeira@hotmail.com>
2024-11-02 04:50:04 +0000

    add settings / reports and prefs to navbar remove navbar on mobile

---
+++ src/routes/(app)/map/settings/preferences/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+    import { dev } from '$app/environment';
+</script>
+<iframe style="width: 100%; height: 100%" title="map" src="{dev?'http://localhost:3000':''}/map/settings/preferences"></iframe>
