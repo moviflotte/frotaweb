5d9c11031793ea33cb07aaaa4b7227036bb15b43
jcardus <joaquim_cardeira@hotmail.com>
2024-11-02 09:23:07 +0000

    children

--- src/routes/(app)/map/+layout.svelte
+++ src/routes/(app)/map/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
     import { dev } from '$app/environment';
     import {page} from "$app/stores";
+    let { children } = $props();
 </script>
 <iframe style="width: 100%; height: 100%" title="map"
         src="{dev?'http://localhost:3000':''}{$page.url.pathname.replace('/main', '')}">
 </iframe>
+{@render children()}
