046b912185c26f2cb4b1f193dddb9d7dfcb58f74
jcardus <joaquim_cardeira@hotmail.com>
2024-11-02 08:22:16 +0000

    use tailwing gray on traccar sync media sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '900px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ['Roboto', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif'], // Custom font stack
--- traccar-patch/src/common/theme/palette.js
+++ traccar-patch/src/common/theme/palette.js
@@ -2,12 +2,24 @@ import { grey, green, indigo } from '@mui/material/colors';
 
 const validatedColor = (color) => (/^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null);
 
-const tailwindGray800 = '#1f2937'
+const tailWindGray = {
+  50: '#f9fafb',
+      100: '#f3f4f6',
+      200: '#e5e7eb',
+      300: '#d1d5db',
+      400: '#9ca3af',
+      500: '#6b7280',
+      600: '#4b5563',
+      700: '#374151',
+      800: '#1f2937',
+      900: '#111827',
+}
+
 export default (server, darkMode) => ({
   mode: darkMode ? 'dark' : 'light',
   background: {
-    default: darkMode ? tailwindGray800 : grey[50],
-    paper: tailwindGray800
+    default: darkMode ? tailWindGray['900'] : grey[50],
+    paper: darkMode ? tailWindGray[800] : tailWindGray[100],
   },
   primary: {
     main: validatedColor(server?.attributes?.colorPrimary) || (darkMode ? indigo[200] : indigo[900]),
