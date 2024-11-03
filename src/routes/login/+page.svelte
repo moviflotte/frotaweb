b66a48033e87d6e456ce5b250410108fdd976161
jcardus <joaquim_cardeira@hotmail.com>
2024-11-03 03:11:31 +0000

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,28 @@
 <script>
     import '../../app.css'
+    import {goto} from "$app/navigation";
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const fetchData = async () => {
+            {
+                const query = `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password.value)}`;
+                const response = await fetch('/api/session', {
+                    method: 'POST',
+                    body: new URLSearchParams(query),
+                });
+                if (response.ok) {
+                    await goto('/main')
+                } else {
+                    alert(await response.text());
+                }
+            }
+        }
+        fetchData();
+    };
+
+    let email = $state('')
+    let password = $state('')
 </script>
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -7,14 +30,13 @@
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" onsubmit={handleSubmit}>
             <div>
                 <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm/6">
+                    <input bind:value="{email}" id="email" name="email" type="text" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm/6">
                 </div>
             </div>
-
             <div>
                 <div class="flex items-center justify-between">
                     <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
@@ -23,7 +45,7 @@
                     </div>
                 </div>
                 <div class="mt-2">
-                    <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm/6">
+                    <input bind:this="{password}" id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm/6">
                 </div>
             </div>
 
