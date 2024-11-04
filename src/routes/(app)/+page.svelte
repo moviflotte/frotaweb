09c230e13fabf5c5984df61ed044d148afef77bc
jcardus <asklocation.net@gmail.com>
2024-11-04 08:23:46 +0000

    redirect on mobile

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import config from 'tailwindcss/defaultTheme';
+    if (!window.matchMedia(`(min-width: ${config.screens.md})`).matches) {
+        window.location = '/map'
+    }
+</script>
 <iframe style="width: 100%; height: 100%" class="bg-white" title="dashboard"
         src="/dash">
 </iframe>
