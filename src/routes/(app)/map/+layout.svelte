0633bb24c4b321e0515a5e44a783b6d864dd2b31
jcardus <asklocation.net@gmail.com>
2024-11-14 01:56:19 +0000

    don't reload traccar when changing from map to reports and settings

--- src/routes/(app)/map/+layout.svelte
+++ src/routes/(app)/map/+layout.svelte
@@ -1,7 +1,25 @@
 <script>
     import {page} from "$app/stores";
+    import {derived} from 'svelte/store';
+    import {onMount} from "svelte";
     let { children } = $props();
+    let iframeRef;
+
+    const pathname = derived(page, ($page) => $page.url.pathname);
+
+    onMount(() => {
+        pathname.subscribe((_path) => {
+            const path = _path.replace('/map', '/')
+            console.log(_path, path)
+            if (iframeRef && iframeRef.contentWindow) {
+                iframeRef.contentWindow.postMessage({type: 'navigate', path}, '*');
+            } else {
+                console.warn("iframeRef is not yet initialized.", iframeRef);
+            }
+        })
+    })
+
 </script>
-<iframe style="width: 100%; height: 100%" title="map" src="{$page.url.pathname.replace('/map', '/traccar')}">
+<iframe bind:this={iframeRef} style="width: 100%; height: 100%" title="map" src="{window.location.pathname.replace('/map', '/traccar')}">
 </iframe>
 {@render children()}
