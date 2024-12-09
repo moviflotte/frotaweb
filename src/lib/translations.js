32098f699ec521d5dc80a9f5ea626ecb214f89dd
entrack-plataforma <entrack.plataforma@gmail.com>
2024-12-09 23:42:47 +0000

    Update translations.js

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -6,6 +6,7 @@ export default {
         'Password': 'Contraseña',
         'Esqueceu a senha?': 'Olvidó su contraseña?',
         'Não é cliente?': 'No eres cliente?',
-        'Experimente grátis durante 14 dias': 'Prueba grátis por 14 dias'
+        'Experimente grátis durante 14 dias': 'Prueba grátis por 14 dias',
+        'Relatórios Gestão': 'Informes Gestión'
     },
 };
