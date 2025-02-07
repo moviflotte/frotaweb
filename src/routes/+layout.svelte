b40d3d51f18202a3171ee585a2c275ca4aafbd7f
moviflotte <136280595+moviflotte@users.noreply.github.com>
2025-02-07 16:52:20 +0000

    using runes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import { pwaInfo } from 'virtual:pwa-info'; 
 
-  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
+  let webManifestLink = $state(pwaInfo ? pwaInfo.webManifest.linkTag : '') 
   let { children } = $props();
 </script> 
   
