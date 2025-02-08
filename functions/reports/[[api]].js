9614594ead3b3e1f84a1d588591c9ccf510869e9
moviflotte <136280595+moviflotte@users.noreply.github.com>
2025-02-08 19:35:57 +0000

    Update [[api]].js

--- functions/reports/[[api]].js
+++ functions/reports/[[api]].js
@@ -1,5 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
-    url.host = 'traccar-reports.pages.dev'
+    url.host = 'traccar-reports-5zo.pages.dev'
     return fetch(new Request(url, request), cf)
 }
