3cba9ea2d3262a7e3ff164fdc270cb8a51e6ac6e
jcardus <asklocation.net@gmail.com>
2025-02-10 15:38:03 +0000

    title

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    let title = ''
+    fetch(`https://raw.githubusercontent.com/rastreosat/rastreosat.github.io/refs/heads/main/${window.location.hostname}/info.json`).then(r => {
+        if (r.ok) {
+            r.json().then(j => title = j.title)
+        }
+    })
+</script>
+
+<svelte:head>
+    <title>{title}
+    </title>
+</svelte:head>
+
+<slot></slot>
