96631d8213a26bdc57971099f8a22078edc6d522
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-04-10 22:32:26 +0100

    manifest link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
     let title = '', favicon = ''
-    fetch(`https://raw.githubusercontent.com/rastreosat/rastreosat.github.io/refs/heads/main/${window.location.hostname}/info.json`).then(r => {
+    fetch(`https://raw.githubusercontent.com/rastreosat/rastreosat.github.io/refs/heads/main/${
+        window.location.hostname}/info.json`).then(r => {
         if (r.ok) {
             r.json().then(j => {
                 title = j.title
@@ -10,7 +11,7 @@
     })
     import { pwaInfo } from 'virtual:pwa-info';
 
-    $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+    const webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
     window.addEventListener("message", (event) => {
         if (event.data.type === "openUrl") {
             window.location = event.data.url
