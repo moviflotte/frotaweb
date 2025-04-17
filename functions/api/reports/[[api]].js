f7c102e51e49311b33b4edc7786e8dabc3320afc
jcardus <joaquim_cardeira@hotmail.com>
2025-04-17 13:01:23 +0100

    reports need https

--- functions/api/reports/[[api]].js
+++ functions/api/reports/[[api]].js
@@ -1,7 +1,9 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
     url.host = env.TRACCAR_REPORTS || env.TRACCAR_SERVER
-    url.protocol = 'http:'
-    url.port = 80
+    if (!env.TRACCAR_REPORTS) {
+        url.protocol = 'http:'
+        url.port = 80
+    }
     return fetch(new Request(url, request), cf)
 }
