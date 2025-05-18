284de1b5add339836046be1a9a6fee261826bd61
jcardus <joaquim_cardeira@hotmail.com>
2025-05-18 19:21:40 +0100

    fix cesium path

--- src/routes/(app)/ces/+page.svelte
+++ src/routes/(app)/ces/+page.svelte
@@ -1,3 +1,3 @@
 <iframe style="width: 100%; height: 100%" class="bg-white" title="cesium"
-        src="/traccar-cesium{window.location.search}">
+        src="/traccar-cesium/{window.location.search}">
 </iframe>
