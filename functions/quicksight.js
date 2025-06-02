ded21b80770faeacaf696b290160f8ff4c24b29b
alkon-integration <139714353+alkon-integration@users.noreply.github.com>
2025-06-03 00:05:04 +0100

    clean

--- functions/quicksight.js
+++ functions/quicksight.js
@@ -1,43 +1,24 @@
 import { AwsClient } from 'aws4fetch';
 
-const REGION = 'us-east-1';
-const ACCOUNT_ID = '123456789012';
-const DASHBOARD_ID = 'dashboard-id';
+const region = 'us-east-1';
+const ACCOUNT_ID = '137295066015';
+const DASHBOARD_ID = 'ef6b033c-8783-4ad0-b0bd-12ce4834b334';
 
 export const onRequest = async ({env}) => {
     const aws = new AwsClient({
-        accessKeyId: env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: env?.AWS_ACCESS_KEY_ID,
+        secretAccessKey: env?.AWS_SECRET_ACCESS_KEY,
         service: 'quicksight',
-        region: REGION
+        region
     });
-    const body = JSON.stringify({
-        AwsAccountId: ACCOUNT_ID,
-        SessionLifetimeInMinutes: 60,
-        Namespace: 'default',
-        AuthorizedResourceArns: [
-            `arn:aws:quicksight:${REGION}:${ACCOUNT_ID}:dashboard/${DASHBOARD_ID}`
-        ],
-        ExperienceConfiguration: {
-            Dashboard: {
-                InitialDashboardId: DASHBOARD_ID
-            }
-        }
+    const body = new URLSearchParams({
+        'creds-type': 'QUICKSIGHT',
+        'user-arn': `arn:aws:quicksight:${region}:${ACCOUNT_ID}:user/default/${ACCOUNT_ID}`
     });
+    const url = `https://quicksight.${region}.amazonaws.com/accounts/${ACCOUNT_ID}/dashboards/${DASHBOARD_ID}/embed-url?${body}`
+    const response = await aws.fetch(url)
 
-    const response = await aws.fetch(
-        `https://quicksight.${REGION}.amazonaws.com/accounts/${ACCOUNT_ID}/embed-url/anonymous`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Amz-Target': 'QuickSight.GenerateEmbedUrlForAnonymousUser'
-            },
-            body
-        }
-    );
+    const {EmbedUrl} = await response.json();
 
-    return new Response(await response.text(), {
-        headers: { 'Content-Type': 'application/json' }
-    });
+    return new Response(EmbedUrl);
 }
