bc31e73c649293598598915e2d952e58471bcb57
alkon-integration <139714353+alkon-integration@users.noreply.github.com>
2025-06-03 00:13:40 +0100

    quicksight page

---
+++ src/routes/(app)/licenses/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    let url = $state('')
+    fetch('/quicksight').then(r => r.text()).then(r => (url = r))
+</script>
+<iframe style="width: 100%; height: 100%" class="bg-white" title="licenses"
+        src="{url}">
+</iframe>
+
