633203eb9f55bc89bfa7840a75f92a6c2d5faeb4
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-07-05 23:51:36 +0100

    athena

---
+++ functions/api/reports/athena.js
@@ -0,0 +1,5 @@
+export const onRequest = ({request, env}, cf) => {
+    const url = new URL(request.url)
+    url.host = 'aadobrygc6wsyawaleatkimjjm0cczwu.lambda-url.us-east-1.on.aws'
+    return fetch(new Request(url, request), cf)
+}
