fcd20f1308305216af9235c64e77469e457e6387
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-07-05 23:54:29 +0100

    athena 2

--- functions/api/reports/athena.js
+++ functions/api/reports/athena.js
@@ -1,5 +1,6 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
     url.host = 'aadobrygc6wsyawaleatkimjjm0cczwu.lambda-url.us-east-1.on.aws'
+    url.pathname.replace('athena', 'route')
     return fetch(new Request(url, request), cf)
 }
