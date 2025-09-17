0a7cac7f167bec132716015707391a2762e1c84e
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-09-17 16:48:49 +0100

    debug

--- functions/api/reports/athena.js
+++ functions/api/reports/athena.js
@@ -1,5 +1,7 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
+    const from = url.searchParams.get('from')
+    console.log('from query parameter:', from)
     url.host = 'aadobrygc6wsyawaleatkimjjm0cczwu.lambda-url.us-east-1.on.aws'
     url.pathname.replace('athena', 'route')
     return fetch(new Request(url, request), cf)
