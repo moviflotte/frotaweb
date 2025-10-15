a67e8c34c277b1a2cbcf1506a9ef85a80772b6db
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-10-15 01:26:16 +0100

    chatkit.js

--- functions/chat/[[chat]].js
+++ functions/chat/[[chat]].js
@@ -1,6 +1,6 @@
 export const onRequest = ({request}, cf) => {
     const url = new URL(request.url)
-    url.host = 'chatgpt-worker.entrack-plataforma.workers.dev'
+    url.host = 'traccar-chatkit.pages.dev'
     url.port = 443
     url.protocol = 'https:'
     return fetch(new Request(url, request), cf)
