17e808b850a73a5fb279a79106da465dd382e167
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-10-22 11:04:47 -0300

    TypeError Failed to fetch

--- src/routes/(app)/+layout.js
+++ src/routes/(app)/+layout.js
@@ -1,10 +1,15 @@
 import {goto} from "$app/navigation";
 export const ssr = false
 export const load = async (event) => {
-    const response = await event.fetch('/api/session');
-    if (response.ok) {
-        return {session: await response.json()};
-    } else if (!window.location.href.endsWith('session/openid/auth')) {
-        await goto('/login')
+    try {
+        const response = await event.fetch('/api/session');
+        if (response.ok) {
+            return {session: await response.json()};
+        } else if (!window.location.href.endsWith('session/openid/auth')) {
+            await goto('/login')
+        }
+    } catch (e) {
+        alert(e.message)
+        console.error(e)
     }
 };
