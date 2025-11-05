fc1cbbebc8abb5eba477b78b8696761e3faaba88
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-05 18:39:39 +0000

    use token

--- src/routes/(app)/+layout.js
+++ src/routes/(app)/+layout.js
@@ -4,7 +4,12 @@ export const load = async (event) => {
     try {
         const response = await event.fetch('/api/session');
         if (response.ok) {
-            return {session: await response.json()};
+            const body = new URLSearchParams();
+            body.append('expiration', new Date(new Date().getTime()+1000*60*60*24).toISOString());
+            const token = await fetch('/api/session/token',
+                {method: 'post', body, headers: {'Content-Type': 'application/x-www-form-urlencoded'} })
+                .then(r => r.text())
+            return {session: await response.json(), token};
         } else if (!window.location.href.endsWith('session/openid/auth')) {
             await goto('/login')
         }
--- src/routes/(app)/temp/+page.svelte
+++ src/routes/(app)/temp/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    const {data} = $props()
+</script>
+
 <iframe style="width: 100%; height: 100%" class="bg-white" title="temperature"
-        src="/traccar-temperature">
+        src="/traccar-temperature?token={data.token}">
 </iframe>
